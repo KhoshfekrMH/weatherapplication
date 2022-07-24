b2c97c2a74993159ca896fb284472d8689f2f45a
brainy2001 <khoshfekr2001@protonmail.com>
2022-07-24 22:11:52 +0430

    I test weather of London and make a https variable and add API url from open weather website to test it work in terminal or not

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/" , function (req,res) {
+   const url = "https://api.openweathermap.org/data/2.5/weather?q=londen&units=metric&appid=b3b1be3801e0620097ba0e60ff6343e5";
+
+   https.get(url, function (response) {
+      console.log(response);
+   });
+
    res.send("server is running");
 });
 
