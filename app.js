8fa47c1451a5028d425c81704bdfc24b1d8cafdc
brainy2001 <khoshfekr2001@protonmail.com>
2022-07-25 01:22:45 +0430

    now server can send index.html

--- app.js
+++ app.js
@@ -6,30 +6,33 @@ const app = express();
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/" , function (req,res) {
-   const url = "https://api.openweathermap.org/data/2.5/weather?q=londen&units=metric&appid=b3b1be3801e0620097ba0e60ff6343e5";
-
-   https.get(url, function (response) {
-      console.log(response);
-      console.log(response.statusCode);
-      response.on("data",function (data) {
-         const weatherData = JSON.parse(data);
-         const temperature = weatherData.main.temp;
-         const weatherDescription = weatherData.weather[0].description;
-         const icon = weatherData.weather[0].icon;
-         const imageURL = " http://openweathermap.org/img/wn/" + icon + "@2x.png";
-
-         console.log(weatherDescription);
-         res.write("<p>The Temperature in London is " + temperature + " degrees Celcuis.</p>");
-         res.write("<h1>The weather is currently " + weatherDescription + " .</h1>");
-         res.write("<img src="+ imageURL +" >");
-         res.send();
-      });
-
-   });
-
+   res.sendFile(__dirname + "/index.html");
 });
 
 
 app.listen(3000,function () {
    console.log("server is running out on port 3000.");
 });
+
+
+/*
+const url = "https://api.openweathermap.org/data/2.5/weather?q=londen&units=metric&appid=b3b1be3801e0620097ba0e60ff6343e5";
+
+https.get(url, function (response) {
+   console.log(response);
+   console.log(response.statusCode);
+   response.on("data",function (data) {
+      const weatherData = JSON.parse(data);
+      const temperature = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = " http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      console.log(weatherDescription);
+      res.write("<p>The Temperature in London is " + temperature + " degrees Celcuis.</p>");
+      res.write("<h1>The weather is currently " + weatherDescription + " .</h1>");
+      res.write("<img src="+ imageURL +" >");
+      res.send();
+   });
+
+});*/
