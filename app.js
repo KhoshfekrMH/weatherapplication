290f197f5503212c1a5a8d0f121e3c94696a61d0
brainy2001 <khoshfekr2001@protonmail.com>
2022-07-24 22:02:29 +0430

    I make app.get and app.listen on port 3000 in app.js and server running successfully

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended : true}));
 
+app.get("/" , function (req,res) {
+   res.send("server is running");
+});
+
+
+app.listen(3000,function () {
+   console.log("server is running out on port 3000.");
+});
