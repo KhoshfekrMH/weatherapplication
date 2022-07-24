7f55bb0e08d76b6c2cf17cc3a0cc3aa08edacfb4
brainy2001 <khoshfekr2001@protonmail.com>
2022-07-25 01:33:28 +0430

    now User can type their city and choose their temperature unit and get their city's weather

--- app.js
+++ app.js
@@ -9,6 +9,33 @@ app.get("/" , function (req,res) {
    res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function (req,res) {
+   const city = req.body.cityInput;
+   const unit = req.body.tempUnit;
+   const APIkey = "b3b1be3801e0620097ba0e60ff6343e5";
+
+   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + unit + "&appid=" + APIkey;
+   
+   https.get(url,function (response) {
+      console.log(response.statusCode);
+
+      response.on("data", function (data) {
+         const weatherData = JSON.parse(data);
+         const temperature = weatherData.main.temp;
+         const weatherDescription = weatherData.weather[0].description;
+         const icon = weatherData.weather[0].icon;
+         const imageURL = " http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+         res.write("<p>The Temperature in London is " + temperature + " degrees Celcuis.</p>");
+         res.write("<h1>The weather is currently " + weatherDescription + " .</h1>");
+         res.write("<img src="+ imageURL +" >");
+         res.send();
+      });
+
+   });
+   
+});
+
 
 app.listen(3000,function () {
    console.log("server is running out on port 3000.");
@@ -16,7 +43,7 @@ app.listen(3000,function () {
 
 
 /*
-const url = "https://api.openweathermap.org/data/2.5/weather?q=londen&units=metric&appid=b3b1be3801e0620097ba0e60ff6343e5";
+
 
 https.get(url, function (response) {
    console.log(response);
